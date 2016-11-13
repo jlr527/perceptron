4fa1e05adb1271f7802bea909876c4ee700d426b
Julian Stiven Rivera <jlr527@hotmail.com>
2016-11-12 22:09:32 -0500

    fix

--- app/controller/IndexController.js
+++ app/controller/IndexController.js
@@ -11,32 +11,67 @@ app.controller('IndexController', function ($scope) {
 				w3: this.random()
 			}
 		},
-		resolverEcuacionGeneral: function(x1, w1, x2, w2, t) {
+		resolverEcuacionActivacion: function(x1, x2, w1, w2, t) {
 			return (x1*w1)+(x2*w2)+(-1*t);
 		},
-		resolverEcuacionpeso: function(w, e, x1, x2) {
+		resolverEcuacionpeso: function(w, x1, x2) {
+			var e = $scope.neurona.e;
 			return w+(e*x1*x2);
+		},
+		resolver: function(number) {
+			if(number >= 0){
+				return 1;
+			}
+			return -1;
+		},
+		aprender: function(tipo) {
+			var util = this;
+			var salir = false;
+			var funcion;
+			var pesos = $scope.neurona.pesos;
+
+			switch(tipo){
+				case 'and':
+					funcion = $scope.funcion.and;
+				break;
+
+				case 'or':
+					funcion = $scope.funcion.or;
+				break;
+
+				case 'xor':
+					funcion = $scope.funcion.xor;
+				break;
+			}
+
+			for(var i = 0, i < funcion.lenght; i++) {
+				var value = funcion[i];
+				var result = util.resolverEcuacionActivacion(value.x1, value.x2, pesos.w1, pesos.w2, pesos.w3);
+
+				if(util.resolver(result) == -1){
+					pesos.w1 = util.resolverEcuacionpeso(pesos.w1, value.y, value.x1);
+					pesos.w2 = util.resolverEcuacionpeso(pesos.w2, value.y, value.x2);
+					pesos.w3 = util.resolverEcuacionpeso(pesos.w1, value.y, -1);
+					i = 0;
+				}
+			};
+			
 		}
 	};
 
 	$scope.funcion = {
 		and:[{x1: 1, x2: 1, y: 1 },
-			 {x1: 1, x2: 0, y: 0 },
-			 {x1: 0, x2: 1, y: 0 },
-			 {x1: 0, x2: 0, y: 0 }],
+			 {x1: 1, x2: 0, y: -1 },
+			 {x1: 0, x2: 1, y: -1 },
+			 {x1: 0, x2: 0, y: -1 }],
 
 		or: [{x1: 1, x2: 1, y: 1 },
 			 {x1: 1, x2: 0, y: 1 },
 			 {x1: 0, x2: 1, y: 1 },
-			 {x1: 0, x2: 0, y: 0 }]
+			 {x1: 0, x2: 0, y: -1 }]
 	};
 	$scope.neurona = {
 		e: 0.5,
-		entrada: {
-			x1: null,
-			x2: null,
-			x3: null
-		},
 		pesos: {
 			w1: utilities.random(),
 			w2: utilities.random(),
